96e71074d497afb85cc0acf821db223051a6b683
Akshar Bhatnagar <bhatnagarakshar@gmail.com>
2025-01-15 00:38:51 +0530

    Starting DRF

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,7 @@
 from django.http import JsonResponse
 import json
 
+from rest_framework.response import Response
+
 def api_home(request, *args, **kwargs):
-    body=request.body  #byte string of JSON data
-    data={}
-    dictionary_params=dict(request.GET)
-    print(f"this is query paramaters",dictionary_params)
-    try:
-        data=json.loads(body)
-    except:
-        pass
-    print(data)
-    return JsonResponse(data)
+    return JsonResponse({"hi":"akshar"})
